cf2fd0de6529127ade1fc1440bfb3c64bf1c5f74
Divyesh Reddy.E <divyeshreddy2003@gmail.com>
2023-08-13 21:51:58 +0530

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -1,10 +1,6 @@
-BFS algorithm in C++
-
 #include <iostream>
 #include <list>
-
 using namespace std;
-
 class Graph {
   int numVertices;
   list<int>* adjLists;
@@ -20,21 +16,16 @@ Graph::Graph(int vertices) {
   numVertices = vertices;
   adjLists = new list<int>[vertices];
 }
-
-// Add edges to the graph
 void Graph::addEdge(int src, int dest) {
   adjLists[src].push_back(dest);
   adjLists[dest].push_back(src);
 }
 
-// BFS algorithm
 void Graph::BFS(int startVertex) {
   visited = new bool[numVertices];
   for (int i = 0; i < numVertices; i++)
     visited[i] = false;
-
   list<int> queue;
-
   visited[startVertex] = true;
   queue.push_back(startVertex);
 
@@ -42,7 +33,7 @@ void Graph::BFS(int startVertex) {
 
   while (!queue.empty()) {
     int currVertex = queue.front();
-    cout << "Visited " << currVertex << " ";
+    cout <<  currVertex << " ";
     queue.pop_front();
 
     for (i = adjLists[currVertex].begin(); i != adjLists[currVertex].end(); ++i) {
@@ -64,7 +55,7 @@ int main() {
   g.addEdge(2, 3);
   g.addEdge(3, 3);
 
-  g.BFS(2);
+  g.BFS(0);
 
   return 0;
 }
